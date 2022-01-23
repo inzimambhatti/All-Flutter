ffa21c5637b628503bb3ccee8850af5b0db5be0b
Inzimam Bhatti <54174389+inzimambhatti@users.noreply.github.com>
2022-01-22 23:48:14 -0800

    output

--- signup-page-in flutter/lib/register.dart
+++ signup-page-in flutter/lib/register.dart
@@ -5,6 +5,29 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void _showDialog() {
+      // flutter defined function
+      showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          // return object of type Dialog
+          return AlertDialog(
+            title: new Text("Success"),
+            content: new Text("Account is created successfully"),
+            actions: <Widget>[
+              // usually buttons at the bottom of the dialog
+              new FlatButton(
+                child: new Text("Close"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+
     return MaterialApp(
         home: Scaffold(
           backgroundColor: Colors.grey,
@@ -16,111 +39,113 @@ class MyApp extends StatelessWidget {
               color: Colors.white,
             ),
           ),
-          body: SafeArea(
-            child: Padding(
-              padding: const EdgeInsets.only(left: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text("Register",
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.w800,
-                        color: Colors.white,
-                      )),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Text("Create new account to",
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.w400,
-                        color: Colors.white,
-                      )),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Text("get Started.",
-                      style: TextStyle(
-                        fontSize: 25,
-                        fontWeight: FontWeight.w400,
-                        color: Colors.white,
-                      )),
-                  SizedBox(
-                    height: 60,
-                  ),
-                  Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(right: 20, left: 20),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Name',
-                              fillColor: Colors.white),
+          body: SingleChildScrollView(
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Register",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w800,
+                          color: Colors.white,
+                        )),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text("Create new account to",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white,
+                        )),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Text("get Started.",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white,
+                        )),
+                    SizedBox(
+                      height: 60,
+                    ),
+                    Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(right: 20, left: 20),
+                          child: TextField(
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Name',
+                                fillColor: Colors.white),
+                          ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 20, left: 20),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: "Email",
-                              fillColor: Colors.white),
+                        SizedBox(
+                          height: 20,
                         ),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 20, left: 20),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Phone',
-                              fillColor: Colors.white),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 20, left: 20),
+                          child: TextField(
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: "Email",
+                                fillColor: Colors.white),
+                          ),
                         ),
-                      ),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 20, left: 20),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.white)),
-                              labelText: 'Password',
-                              fillColor: Colors.white),
+                        SizedBox(
+                          height: 20,
                         ),
-                      ),
-                      SizedBox(
-                        height: 120,
-                      ),
-                      Text("       Already have an account?Sign Up",
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w300,
-                          )),
-                      SizedBox(
-                        height: 20,
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(right: 20, left: 20),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText:
-                              '                                  Register',
-                              fillColor: Colors.white),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 20, left: 20),
+                          child: TextField(
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'Phone',
+                                fillColor: Colors.white),
+                          ),
                         ),
-                      ),
-                    ],
-                  )
-                ],
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 20, left: 20),
+                          child: TextField(
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                    borderSide: BorderSide(color: Colors.white)),
+                                labelText: 'Password',
+                                fillColor: Colors.white),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 120,
+                        ),
+                        Text("       Already have an account?sign in",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w300,
+                            )),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 20, left: 20),
+                          child: ElevatedButton(
+                            onPressed: () {
+                             _showDialog();
+                            },
+                            child: Text("Sign Up"),
+
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
               ),
             ),
           ),
